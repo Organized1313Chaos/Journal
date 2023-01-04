bdf789c50a2bc61fdbfa8e9cd80af5849de29fe8
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-04 17:49:38 +0530

    added textarea to take input

--- djangoproject/djangoproject/views.py
+++ djangoproject/djangoproject/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 # Hello world API
 def index(request):
-    context = {'name': 'Chaos', 'type': 'Organized'}
     
     #render returns a template and context dictionary
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
