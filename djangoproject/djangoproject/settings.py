0b40bbaee4812d3617ffe74331cf3d15094c07d8
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-05 16:35:12 +0530

    environment variables added

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -15,12 +15,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0lq$#*%!hsj=@9mr6gmrrs_ix62(9l(fk!@h9+)^(pmq@aqw$='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
