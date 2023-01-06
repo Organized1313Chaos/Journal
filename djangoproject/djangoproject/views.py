23c88c612499f4a5ee20ef593eb06cb34d59446a
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-06 16:06:40 +0530

    added navigation links and punctuation remover function

--- djangoproject/djangoproject/views.py
+++ djangoproject/djangoproject/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import re
 
 # Hello world API
 def index(request):
+    google = r"https://google.com"
+    leetcode = r"https://leetcode.com/"
+   
+    params = {'google': google, 'leetcode': leetcode}
     #render returns a template and context dictionary
-    return render(request, 'index.html')
+    return render(request, 'index.html', params)
 
-def removepunc(request):
+def analyze(request):
     text = request.GET.get('text', 'default')
-    return HttpResponse("removing punctuations..."+f"===========> {text}")
+    punctuation = request.GET.get('punctuation', 'off')
+    
+    if punctuation=='on':
+        text = re.sub(r'[^\w\s]', '',  text)
+        return HttpResponse(f"{text}")
+    
+    return HttpResponse('Error')
