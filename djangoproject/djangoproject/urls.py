87f47921ad57c5198656d0f709c6d25d920d1503
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-06 16:05:44 +0530

    changed endpoint to analyze

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('',views.index ,name='index'),
     path('admin/', admin.site.urls),
-    path('removepunc/',views.removepunc ,name='removepunc'),
+    path('analyze/',views.analyze ,name='analyze'),
 ]
