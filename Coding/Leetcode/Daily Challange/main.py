be7d525e7a027b8f87ad57d5192ecc74752bcecd
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-09 11:27:41 +0530

    Contest Question: 6289, failed Approach

--- Coding/Leetcode/Daily Challange/main.py
+++ Coding/Leetcode/Daily Challange/main.py
@@ -1,7 +1,6 @@
 '''
 Leetcode Competition: Biweekly Contest
 Date: January 7, 2023
-
 Problem Number: 6289
 Name: Find Xor-Beauty of Array
 '''
@@ -9,19 +8,21 @@ Name: Find Xor-Beauty of Array
 '''
 Failed Approach 1: Brute Force
 '''
+
 class Solution:
     def xorBeauty(self, nums) -> int:
         triplets = []
-        total = 0
         for i in nums:
             for j in nums:
                 for k in nums:
                     triplets.append( (i,j,k) )
-                    total = total ^  ((i | j) & k)
                     
-        return total       
+        for p in triplets:
+            for q in triplets:
+                if ( (p[0]| p[1])&p[2] ) ^ ( (q[0]| q[1])&q[2] )==1:
+                    print(f"{p} ^ {q} ==> {( (p[0]| p[1])&p[2] ) ^ ( (q[0]| q[1])&q[2] )}") 
             
 sol = Solution()
-nums = [15,45,20,2,34,35,5,44,32,30]
+nums = [0,1]
 res = sol.xorBeauty(nums)
 print(f"res={res}")
