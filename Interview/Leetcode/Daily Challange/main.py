67fdcba8bad786678e90e98e7f4ca1ac89ab3fda
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-19 12:41:02 +0530

    Main template code

--- Interview/Leetcode/Daily Challange/main.py
+++ Interview/Leetcode/Daily Challange/main.py
@@ -1,31 +1,14 @@
-import heapq
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        heap, ans, = [], [] 
-        for s, e in intervals + [newInterval]: # add start & end to heap (-1 is start, 1 is end)
-            heapq.heappush(heap, (s, -1))
-            heapq.heappush(heap, (e, 1))
-        cur, s = 0, None            
-        while heap:                            
-            i, val = heapq.heappop(heap)       # pop heap
-            if s is None: s = i                # is s is None, assign i to s (interval start)
-            cur += val                         # keep counting until close interval
-            if not cur:                        # when cur == 0, meaning we can close the interval
-                ans.append([s, i])             # append interval to ans
-                s = None                       # reset s to None
-        return ans        
-
 
 '''
 Leetcode Question
 Date: January 11, 2023
-Problem Number: 57
-Name: Insert Interval 
+Problem Number: 974
+Name: Subarray Sums Divisible by K
 '''
 
 '''
-Approach 1: Partially Successful, redundent approach. 
-No of Test Cases Passed: 146/156
+Approach 1: Partially Successful, Brute force approach. 
+No of Test Cases Passed: 38/73
 '''
 
 '''
@@ -33,60 +16,81 @@ NOTES:
 pass
 
 MISTAKES:
-Reason Of Failure: Not able to cover the edge cases.
+Reason Of Failure: nums[i:j+1], earlier used--> nums[i:j]
+j range--> If pairwise comparison is needed then we use i+1
+       --> If all elements are needed, we need it from i 
 '''
-class Solution:
-    def insert(self, intervals, newInterval):
-        if not intervals: return [newInterval]
-        N = len(intervals)
-        start, end =newInterval
-        
-        index = 0
-        
-        
-        for s, e in intervals:
-            if start>e:
-                index +=1
-                continue
-            break
-        # print(index)
-        try:
-            min_ = min( start, intervals[index][0] )
-        except:
-            min_ = start
 
-        index2 = N-1
+class Solution:
+    def subarraysDivByK(self, nums: list[int], k: int) -> int:
+        N = len( nums )
+        output = 0
+        for i in range(N):
+            for j in range(i, N):
+                if sum(nums[i:j+1])%k==0:
+                    # print(nums[i:j+1])
+                    output+=1
         
-        for i in range(N-1,index,-1):
-            if end<intervals[i][0]:
-                index2-=1
-                continue
-            break
-        try:
-            max_ = max( end, intervals[index2][1] )
-        except:
-            max_ = end
-        # print(min_, max_)
+        return output
+ 
+ 
+'''
+Approach 2: Partially Successful, Brute force approach. 
+No of Test Cases Passed: 66/73
+'''
+
+'''
+NOTES:
+- Use of prefix sum
+
+MISTAKES:
+pass
+''' 
+    
+class Solution:
+    def subarraysDivByK(self, nums: list[int], k: int) -> int:
+        N = len( nums )
+        output = 0
+        for i in range(N):
+            prefix = 0
+            for j in range(i, N):
+                prefix += nums[j]
+                if prefix%k==0:
+                    # print(nums[i:j+1])
+                    output+=1
         
-        # Need to handle edge cases here
-        if index==index2:
-            if min_<intervals[index][0]:
-                
-                return intervals[:index] + [newInterval, intervals[index] ] + intervals[index2+1:]
-            
+        return output
+
+'''
+Approach 3: Successful, Optimum approach using prefix sum 
+No of Test Cases Passed: 73/73
+'''
+
+'''
+NOTES:
+- make sure initial remainder is 1 at 0
+
+MISTAKES:
+pass
+''' 
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        remainders = [0]*k
+        remainders[0] =1
+        prefix = 0
+        output = 0
+        for  num in nums:
+            prefix = ( prefix  + num%k +k) % k
+            output +=  remainders[prefix]
+            remainders[prefix] +=1
         
-        ans = intervals[:index] + [[min_,max_]] + intervals[index2+1:]
-        return ans 
-            
+        return output
+    
 sol = Solution()
-intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
-newInterval = [4,8]
-res = sol.insert(intervals, newInterval)
+nums = [4,5,0,-2,-3,1]
+k = 5
+res = sol.subarraysDivByK(nums, k)
 print(f"res={res}")
 
-'''
-p-m
-a - o
-r-s-k
-e -i
-'''
+        
