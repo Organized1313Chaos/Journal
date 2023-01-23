69d566b9b7fa9fada6fd0e84260751f7c6c421e1
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-23 12:30:10 +0530

    Function to check palindrome string

---
+++ Interview/DS/string/isPalindrome.py
@@ -0,0 +1,27 @@
+'''
+Link: https://leetcode.com/problems/valid-palindrome/description/
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        lookup = set('qwertyuiopasdfghjklzxcvbnm1234567890')
+        s = s.lower()
+
+        l, r = 0, len(s) -1 
+
+        while l<r:
+            if s[l] not in lookup:
+                l+=1
+                continue
+            if s[r] not in lookup:
+                r-=1
+                continue
+            
+            if s[l]!=s[r]:
+                return False
+
+            l+=1
+            r-=1
+
+        return True
+
