396b2177a613d698da08fffb654c627a9e2eaef9
Nehul Patel <Nehulbhai.Patel@helloverify.com>
2023-01-25 17:50:14 +0530

    OS library system calls

---
+++ Coding/Python3/Python/os/system_calls.py
@@ -0,0 +1,8 @@
+# os.system(string_command)
+# execute string command, returns exit status  
+
+import os 
+hello =  os.system('echo Hello')
+print(hello)
+all_dirs =  os.system('dir')
+print(all_dirs)
